6680b891805fe6f8bd363144a205c7fdda921af2
bigballahransom <105400568+bigballahransom@users.noreply.github.com>
2024-06-25 08:28:09 -0700

    show more

--- components/custom/Footer.tsx
+++ components/custom/Footer.tsx
@@ -1,51 +1,47 @@
+'use client'
 import Image from 'next/image'
-import Link from 'next/link';
-import React from 'react'
-import { FaGithub } from "react-icons/fa";
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { FaGithub } from "react-icons/fa"
 
 const Footer = () => {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
   return (
     <div>
       <footer className="text-gray-600 body-font">
-  <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-    <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-    <Image src='/logo.png' alt='logo' className='w-10 rounded-full' width={1000} height={1000}/>
-      <span className="ml-3 text-xl">Andrew Ransom</span>
-    </a>
-    <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-      {/* <a className="text-gray-500">
-        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-        </svg>
-      </a> */}
-      {/* <a className="ml-3 text-gray-500">
-        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-        </svg>
-      </a> */}
-      <div className='flex items-center'>
-      <p className="text-sm text-gray-500 sm:mr-4 sm:pr-4 sm:border-r-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 1996 Andrew Ransom —
-      <a href="https://github.com/bigballahransom" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@bigballahransom</a>
-      </p>
-      <Link href='https://github.com/bigballahransom' className="ml-3 text-gray-500 hover:cursor-pointer">
-        {/* <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
-          <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-        </svg> */}
-        <FaGithub size={20} />
-      </Link>
-      <Link href='https://www.linkedin.com/in/andrew-ransom-b36a02266/' className="ml-3 text-gray-500 hover:cursor-pointer">
-        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" className="w-5 h-5" viewBox="0 0 24 24">
-          <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
-          <circle cx="4" cy="4" r="2" stroke="none"></circle>
-        </svg>
-      </Link>
-      </div>
-    </span>
-  </div>
-</footer>
+        <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+          <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+            <Image src='/logo.png' alt='logo' className='w-10 rounded-full' width={1000} height={1000} />
+            <span className="ml-3 text-xl">Andrew Ransom</span>
+          </a>
+          <button
+            className="sm:hidden block mt-4 text-gray-500 focus:outline-none"
+            onClick={() => setIsCollapsed(!isCollapsed)}
+          >
+            {isCollapsed ? 'Show More' : 'Show Less'}
+          </button>
+          <span className={`sm:flex ${isCollapsed ? 'hidden' : 'flex'} flex-col sm:flex-row sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start`}>
+            <div className='flex items-center'>
+              <p className="text-sm text-gray-500 sm:mr-4 sm:pr-4 sm:border-r-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 1996 Andrew Ransom —
+                <a href="https://github.com/bigballahransom" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@bigballahransom</a>
+              </p>
+              <Link href='https://github.com/bigballahransom' className="ml-3 text-gray-500 hover:cursor-pointer">
+                <FaGithub size={20} />
+              </Link>
+              <Link href='https://www.linkedin.com/in/andrew-ransom-b36a02266/' className="ml-3 text-gray-500 hover:cursor-pointer">
+                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" className="w-5 h-5" viewBox="0 0 24 24">
+                  <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+                  <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                </svg>
+              </Link>
+            </div>
+          </span>
+        </div>
+      </footer>
     </div>
   )
 }
 
 export default Footer
+
