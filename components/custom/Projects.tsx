fb5105e943e2254fbb763c8b10382246f81544fd
bigballahransom <105400568+bigballahransom@users.noreply.github.com>
2024-06-29 11:37:22 -0700

    yourmovers

--- components/custom/Projects.tsx
+++ components/custom/Projects.tsx
@@ -109,13 +109,63 @@ const Projects = () => {
                 <Badge variant="outline" className='bg-blue-400 text-white'>AWS SES</Badge>
             </div>
             <div className="flex items-center flex-wrap ">
-              <Link href='https://www.pickupmonster.com' className="text-blue-400 inline-flex items-center md:mb-2 lg:mb-0 hover:cursor-pointer hover:text-blue-500">View Project
+              <Link href='https://www.app.bookqube.com' className="text-blue-400 inline-flex items-center md:mb-2 lg:mb-0 hover:cursor-pointer hover:text-blue-500">View Project
                 <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                   <path d="M5 12h14"></path>
                   <path d="M12 5l7 7-7 7"></path>
                 </svg>
               </Link>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="p-4 md:w-1/3">
+        <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden bg-white">
+          <Image className="lg:h-48 md:h-36 w-full object-cover object-center" src="/heft.png" alt="blog" width={1000} height={1000}/>
+          <div className="p-6">
+            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">COMPANY</h2>
+            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Heft Moving</h1>
+            <p className="leading-relaxed mb-3">Drive sales with process and procedures. B2B and B2C, developed a moving sales machine.</p>
+            <div className='flex flex-wrap gap-2 mb-4'>
+                <Badge variant="outline" className='bg-blue-400 text-white'>Sales Development</Badge>
+                <Badge variant="outline" className='bg-blue-400 text-white'>Lead Generation</Badge>
+                <Badge variant="outline" className='bg-blue-400 text-white'>B2B Sales</Badge>
+                <Badge variant="outline" className='bg-blue-400 text-white'>Sales Training</Badge>
+                <Badge variant="outline" className='bg-blue-400 text-white'>Sales Scripts</Badge>
+            </div>
+            <div className="flex items-center flex-wrap ">
+              <Link href='https://www.heftmoves.com' className="text-blue-400 inline-flex items-center md:mb-2 lg:mb-0 hover:cursor-pointer hover:text-blue-500">View Project
+                <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="p-4 md:w-1/3">
+        <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden bg-white">
+          <Image className="lg:h-48 md:h-36 w-full object-cover object-center" src="/yourmovers.jpeg" alt="blog" width={1000} height={1000}/>
+          <div className="p-6">
+            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">COMPANY</h2>
+            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">YourMovers</h1>
+            <p className="leading-relaxed mb-3">Drive sales with process and procedures. B2B and B2C, developed a moving sales machine.</p>
+            <div className='flex flex-wrap gap-2 mb-4'>
+                <Badge variant="outline" className='bg-blue-400 text-white'>Sales Development</Badge>
+                <Badge variant="outline" className='bg-blue-400 text-white'>Lead Generation</Badge>
+                <Badge variant="outline" className='bg-blue-400 text-white'>B2B Sales</Badge>
+                <Badge variant="outline" className='bg-blue-400 text-white'>Sales Training</Badge>
+                <Badge variant="outline" className='bg-blue-400 text-white'>Sales Scripts</Badge>
+            </div>
+            {/* <div className="flex items-center flex-wrap ">
+              <Link href='https://www.heftmoves.com' className="text-blue-400 inline-flex items-center md:mb-2 lg:mb-0 hover:cursor-pointer hover:text-blue-500">View Project
+                <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </Link>
+            </div> */}
           </div>
         </div>
       </div>
