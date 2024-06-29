71463069b9caa37a0bff620899f2cc0397f0e74e
bigballahransom <105400568+bigballahransom@users.noreply.github.com>
2024-06-29 11:01:36 -0700

    download pdf

--- components/custom/Navbar.tsx
+++ components/custom/Navbar.tsx
@@ -3,44 +3,47 @@ import Image from 'next/image'
 import React from 'react'
 import { useToast } from "@/components/ui/use-toast"
 import { Button } from '../ui/button'
+import jsPDF from 'jspdf';
 
 const Navbar = () => {
+  const [saving, setSaving] = React.useState(false)
   const { toast } = useToast()
+  
+  function saveResume() {
+    setSaving(true);
+    const doc = new jsPDF('portrait', 'px', 'a4', false);
+
+    doc.addImage('https://i.ibb.co/TgMwSCQ/Andrew-Ransom-1.png', 'PNG', 0, 0, 465, 700);
+
+    doc.addPage();
+
+    doc.addImage('https://i.ibb.co/MnhZvQD/Andrew-Ransom-2.png', 'PNG', 0, 0, 465, 700);
+    
+    doc.save('Andrew-Ransom-Resume.pdf');
+    setSaving(false);
+  }
+  
   return (
     <div>
       <header className="text-gray-600 body-font">
-  <div className="container mx-auto flex p-5 flex-row justify-between items-center">
-    <a className="flex title-font font-medium items-center text-gray-900 mb-0">
-      {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" viewBox="0 0 24 24">
-        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg> */}
-      <Image src='/logo.png' alt='logo' className='w-10 rounded-full' width={1000} height={1000}/>
-      <span className="ml-3 text-xl">Andrew Ransom</span>
-    </a>
-    {/* <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-gray-900">First Link</a>
-      <a className="mr-5 hover:text-gray-900">Second Link</a>
-      <a className="mr-5 hover:text-gray-900">Third Link</a>
-      <a className="mr-5 hover:text-gray-900">Fourth Link</a>
-    </nav> */}
-    <Button className="inline-flex text-black items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-0 gap-2" 
-      onClick={() => {
-        toast({
-          title: "Please check back later!",
-          description: "This feature is currently under development.",
-        })
-      }}
-    >
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
-</svg>
-
-      Resume
-    </Button>
-  </div>
-</header>
+        <div className="container mx-auto flex p-5 flex-row justify-between items-center">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-0">
+            <Image src='/logo.png' alt='logo' className='w-10 rounded-full' width={1000} height={1000}/>
+            <span className="ml-3 text-xl">Andrew Ransom</span>
+          </a>
+          <Button className="inline-flex text-black items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-0 gap-2" 
+            onClick={saveResume}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+            </svg>
+            Resume
+          </Button>
+        </div>
+      </header>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
+
